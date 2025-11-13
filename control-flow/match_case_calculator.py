bc828bd82599cdcaf1fb3c1a941f75710caa9b3e
Edidiong Archibong <eddymyk13@gmail.com>
2025-11-13 18:00:05 +0100

    Fix a typo.

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,18 +1,18 @@
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
-operator = input("Choose the operation (+, -, *, /): ")
+operation = input("Choose the operation (+, -, *, /): ")
 
-match operator:
-    case "+" : 
+match operation:
+    case "+": 
         result = first_number + second_number
         print(f"The result is {result}.")
-    case "-" : 
+    case "-": 
         result = first_number - second_number
         print(f"The result is {result}.")
-    case "*" : 
+    case "*": 
         result = first_number * second_number
         print(f"The result is {result}.")
-    case "/" : 
+    case "/": 
         if second_number == 0:
             print("can not divide by 0")
         else:
